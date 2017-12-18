e1852158aa4864e31b7faccfd8eba74c3ff9ef38
Julien Rouzieres <julien.rouzieres@mac.com>
2017-12-18 10:43:20 +0100

    missing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'index' });
+  res.render('index', { title: 'Index' });
 });
 
 router.post('/', (req, res) => {
